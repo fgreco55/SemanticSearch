d0269e778c0b8da28e21a33d929df036297f2647
Frank Greco <fgreco@javasig.com>
2025-10-06 16:42:18 -0400

    No longer needed.
    EmbeddingStore filename now ".ingestor.db" in the user's home directory
    
    Removed the emojis
    
    parameterizes the search criteria and offers verbose=true/false.

--- src/main/java/utilities/Common.java
+++ src/main/java/utilities/Common.java
@@ -1,5 +1,5 @@
 package utilities;
 
 public class Common {
-    public static String VDB_NAME = "/Users/fgreco/.ingestor.db";
+    public static String VDB_NAME = System.getProperty("user.home") + "/" + ".ingestor.db";
 }
