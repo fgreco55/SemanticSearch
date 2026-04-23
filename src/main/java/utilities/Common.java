82f144c253a8f9136c8f33d0a253d477b3e4905a
Frank Greco <fgreco@javasig.com>
2026-04-23 12:21:25 -0400

    Added SetProperties().  Moved from the main driver.

--- src/main/java/utilities/Common.java
+++ src/main/java/utilities/Common.java
@@ -1,5 +1,24 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
 public class Common {
     public static String VDB_NAME = System.getProperty("user.home") + "/" + ".ingestor.db";
+
+    public static Properties SetProperties(String configFile) throws IOException {
+        Properties prop = new Properties();
+        InputStream in = new FileInputStream(configFile);
+
+        prop.load(in);
+
+        for (Enumeration e = prop.propertyNames(); e.hasMoreElements();) {
+            String key = e.nextElement().toString();
+            //System.out.println(key + " = " + prop.getProperty(key));
+        }
+        return prop;
+    }
 }
